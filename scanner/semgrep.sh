932b26aa3b0ae083769e2736c5f2023e23c930cf
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-08-23 18:30:25 +0800

    Updated semgrep

--- scanner/semgrep.sh
+++ scanner/semgrep.sh
@@ -6,12 +6,12 @@ pip install semgrep
 
 echo "Scanning files with semgrep"
 if [ "$FULL_CHECK" = true ]; then
-    semgrep scan . --config auto --json > ./scanner/tmp/result/__semgrep__.json
+    semgrep scan . --config auto --json --exclude ./scanner/tmp/ > ./scanner/tmp/result/__semgrep__.json
 else
     modified_files=$(git diff --name-only HEAD $(git merge-base HEAD remotes/origin/$BRANCH))
     for file in $modified_files; do
         foldername=$(basename "$dir" | sed 's/\//__slash__/g')
         filename=$(basename "$file" | sed 's/\//__slash__/g')
-        semgrep scan $file --config auto --json > "./scanner/tmp/result/__semgrep__${foldername}_${filename}.json"
+        semgrep scan $file --config auto --json --exclude ./scanner/tmp/ > "./scanner/tmp/result/__semgrep__${foldername}_${filename}.json"
     done
 fi
