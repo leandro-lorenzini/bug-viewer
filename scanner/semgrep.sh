d57aba4011f77d81c5d23ad19a1bb2972349d385
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-08-28 14:04:09 +0800

    Improving semgrep scanner

--- scanner/semgrep.sh
+++ scanner/semgrep.sh
@@ -9,9 +9,5 @@ if [ "$FULL_CHECK" = true ]; then
     semgrep scan . --config ./scanner/semgrep.yml --severity ERROR --severity WARNING --json > ./scanner/tmp/result/__semgrep__.json
 else
     modified_files=$(git diff --name-only HEAD $(git merge-base HEAD remotes/origin/$BRANCH))
-    for file in $modified_files; do
-        foldername=$(basename "$dir" | sed 's/\//__slash__/g')
-        filename=$(basename "$file" | sed 's/\//__slash__/g')
-        semgrep scan $file --config ./scanner/semgrep.yml --severity ERROR --severity WARNING --json > "./scanner/tmp/result/__semgrep__${foldername}_${filename}.json"
-    done
+    semgrep scan $modified_files --config ./scanner/semgrep.yml --severity ERROR --severity WARNING --json > "./scanner/tmp/result/__semgrep__${foldername}_${filename}.json"
 fi
