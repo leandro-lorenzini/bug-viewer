06ae8906eac96ce0696589fe151b3a3c8991eecf
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-09-08 16:13:34 +0800

    Update python.sh

--- scanner/python.sh
+++ scanner/python.sh
@@ -7,8 +7,8 @@ pip install bandit
 echo "Scanning Python files"
 if [ "$FULL_CHECK" = true ]; then
     # Run scanner on the whole project
-    bandit -r . -f json > ./scanner/tmp/result/__bandit__.json
+    bandit -r . -f json -o ./scanner/tmp/result/__bandit__.json
 else
     # Run scanner on changed files only
-    bandit -r $(git diff --name-only HEAD $(git merge-base HEAD remotes/origin/$BRANCH) | grep '\.py$') -f json > ./scanner/tmp/result/__bandit__.json
+    bandit -r $(git diff --name-only HEAD $(git merge-base HEAD remotes/origin/$BRANCH) | grep '\.py$') -f json -o ./scanner/tmp/result/__bandit__.json
 fi
