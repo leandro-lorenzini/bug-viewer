2ed9332c38f6ad13729202ec6da8a298e68a1741
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-10-25 15:16:03 +0800

    Update semgrep.sh

--- scanner/semgrep.sh
+++ scanner/semgrep.sh
@@ -6,7 +6,7 @@ pip install semgrep
 
 echo "Scanning files with semgrep"
 if [ "$FULL_CHECK" = true ]; then
-    semgrep scan . --config ./scanner/semgrep.yml --severity ERROR --severity WARNING --json > ./scanner/tmp/result/__semgrep__.json
+    semgrep scan . --config ./scanner/semgrep.yml --exclude='*.js' --exclude='*.py' --severity ERROR --severity WARNING --json > ./scanner/tmp/result/__semgrep__.json
 else
     semgrep scan $MODIFIED_FILES --config ./scanner/semgrep.yml --severity ERROR --severity WARNING --json > "./scanner/tmp/result/__semgrep__${foldername}_${filename}.json"
 fi
